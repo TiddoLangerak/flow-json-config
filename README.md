6f6c3d1e1aa5a9440b5fdfca18337c92add149b1
Tiddo Langerak <tiddolangerak@gmail.com>
2017-06-19 10:24:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Utility that transforms a JSON object into a flowconfig file.
+
+## Usage:
+
+```
+flow-json-config file.json [configPath] > .flowconfig
+```
+
+`configPath` is an optional parameter that causes `flow-json-config` to get
+its configuration from a subfield of the input JSON. This can for example
+be used to put the configuration in the `package.json`:
+
+```
+flow-json-config package.json [configPath] > .flowconfig
+```
+
+## Format
+```js
+{
+	"ignore" : [/* ignore path */],
+	"include" : [/* include paths */],
+	"libs" : [/* lib paths */],
+	"options" : {
+		"optionKey" : "optionValue",
+		"nested" : {
+			"subKey" : "subValue"
+		}
+	}
+}
+```
+
+See the [examples](examples) folder for full examples.
---
+++ index.js
@@ -0,0 +1,70 @@
+var fs = require('fs');
+
+var filePath = process.argv[2];
+var field = process.argv[3];
+var fieldParts = [];
+if (field) {
+	fieldParts = field.split('.');
+}
+
+var file = fs.readFileSync(filePath, 'utf8');
+var json;
+try {
+	json = JSON.parse(file);
+} catch (e) {
+	console.error(filePath + " is not a valid JSON file");
+	process.exit(1);
+}
+var flowConfig = fieldParts
+	.reduce((obj, path) => {
+		if (!obj[path]) {
+			console.error("Could not find flow config in file '" + filePath + "' for field '" + field + "'");
+			process.exit(1);
+		}
+		return obj[path];
+	}, json);
+
+function listToConfig(section, list) {
+	return '[' + section + ']\n' + list.join('\n') + '\n\n';
+}
+
+var output = '# Generated from ' + filePath + '\n';
+if (flowConfig.include) {
+	output += listToConfig('include', flowConfig.include);
+}
+
+if (flowConfig.ignore) {
+	output += listToConfig('ignore', flowConfig.ignore);
+}
+
+if (flowConfig.libs) {
+	output += listToConfig('libs', flowConfig.libs);
+}
+
+function objectToKeyVal(obj, prefix = '') {
+	return Object.entries(obj)
+		.map((tuple) => {
+			var key = tuple[0];
+			var val = tuple[1];
+			if (typeof val === 'object') {
+				return objectToKeyVal(val, prefix + key + ".");
+			}
+			return [
+				prefix + key + '='+val
+			]
+		})
+		.reduce((acc, val) => acc.concat(val))
+}
+
+if (flowConfig.options) {
+	output += listToConfig('options', objectToKeyVal(flowConfig.options));
+}
+
+if (flowConfig.version) {
+	output += listToConfig('version', [flowConfig.version]);
+}
+
+console.log(output);
+
+
+
